90edbd736594228f54119e278ac089a0950d2308
kalicyh <34980061+kalicyh@users.noreply.github.com>
2025-11-28 03:12:46 +0800

    🎈 perf: 移除屏幕

--- main/boards/kalicyh-s3-ml307/kalicyh-s3-ml307.cc
+++ main/boards/kalicyh-s3-ml307/kalicyh-s3-ml307.cc
@@ -1,6 +1,5 @@
 #include "dual_network_board.h"
 #include "codecs/es8311_audio_codec.h"
-#include "display/lcd_display.h"
 #include "application.h"
 #include "button.h"
 #include "config.h"
@@ -17,38 +16,14 @@
 #include <esp_efuse_table.h>
 #include <driver/i2c_master.h>
 
-#include <esp_lcd_panel_io.h>
-#include <esp_lcd_panel_ops.h>
-#include <esp_lcd_gc9a01.h>
-
 #include "driver/gpio.h"
-#include "driver/spi_master.h"
 
 #define TAG "KalicyhS3ML307"
-
-class CustomLcdDisplay : public SpiLcdDisplay {
-public:
-    CustomLcdDisplay(esp_lcd_panel_io_handle_t io_handle, 
-                    esp_lcd_panel_handle_t panel_handle,
-                    int width,
-                    int height,
-                    int offset_x,
-                    int offset_y,
-                    bool mirror_x,
-                    bool mirror_y,
-                    bool swap_xy) 
-        : SpiLcdDisplay(io_handle, panel_handle, width, height, offset_x, offset_y, mirror_x, mirror_y, swap_xy) {
-        // 由于屏幕是圆的，所以状态栏需要增加左右内边距
-        lv_obj_set_style_pad_left(status_bar_, LV_HOR_RES * 0.33, 0);
-        lv_obj_set_style_pad_right(status_bar_, LV_HOR_RES * 0.33, 0);
-    }
-};
 
 class KalicyhS3ML307 : public DualNetworkBoard {
 private:
     i2c_master_bus_handle_t codec_i2c_bus_;
     Button boot_button_;
-    Display* display_;
     PowerSaveTimer* power_save_timer_;
     AdcBatteryMonitor* adc_battery_monitor_ = nullptr;
 
@@ -66,16 +41,10 @@ private:
     void InitializePowerSaveTimer() {
         power_save_timer_ = new PowerSaveTimer(-1, 60, 600);
         power_save_timer_->OnEnterSleepMode([this]() {
-            GetDisplay()->SetPowerSaveMode(true);
-            GetBacklight()->SetBrightness(10);
         });
         power_save_timer_->OnExitSleepMode([this]() {
-            GetDisplay()->SetPowerSaveMode(false);
-            GetBacklight()->RestoreBrightness();
         });
         power_save_timer_->OnShutdownRequest([this]() {
-            GetDisplay()->SetPowerSaveMode(true);
-            GetBacklight()->SetBrightness(0);
         });
         power_save_timer_->SetEnabled(true);
     }
@@ -95,55 +64,6 @@ private:
             },
         };
         ESP_ERROR_CHECK(i2c_new_master_bus(&i2c_bus_cfg, &codec_i2c_bus_));
-    }
-
-    // SPI初始化
-    void InitializeSpi() {
-        ESP_LOGI(TAG, "Initialize SPI bus");
-        spi_bus_config_t buscfg = {
-            .mosi_io_num = DISPLAY_SPI_MOSI_PIN,
-            .miso_io_num = -1,
-            .sclk_io_num = DISPLAY_SPI_SCLK_PIN,
-            .quadwp_io_num = -1,
-            .quadhd_io_num = -1,
-            .data4_io_num = -1,
-            .data5_io_num = -1,
-            .data6_io_num = -1,
-            .data7_io_num = -1,
-            .max_transfer_sz = DISPLAY_WIDTH * DISPLAY_HEIGHT * sizeof(uint16_t),
-            .flags = 0,
-            .isr_cpu_id = ESP_INTR_CPU_AFFINITY_AUTO,
-            .intr_flags = 0
-        };
-        ESP_ERROR_CHECK(spi_bus_initialize(SPI3_HOST, &buscfg, SPI_DMA_CH_AUTO));
-    }
-
-    // GC9A01初始化
-    void InitializeGc9a01Display() {
-        ESP_LOGI(TAG, "Init GC9A01 display");
-
-        ESP_LOGI(TAG, "Install panel IO");
-        esp_lcd_panel_io_handle_t io_handle = NULL;
-        esp_lcd_panel_io_spi_config_t io_config = GC9A01_PANEL_IO_SPI_CONFIG(DISPLAY_SPI_CS_PIN, DISPLAY_SPI_DC_PIN, 0, NULL);
-        io_config.pclk_hz = DISPLAY_SPI_SCLK_HZ;
-        ESP_ERROR_CHECK(esp_lcd_new_panel_io_spi(SPI3_HOST, &io_config, &io_handle));
-    
-        ESP_LOGI(TAG, "Install GC9A01 panel driver");
-        esp_lcd_panel_handle_t panel_handle = NULL;
-        esp_lcd_panel_dev_config_t panel_config = {};
-        panel_config.reset_gpio_num = DISPLAY_SPI_RESET_PIN;    // Set to -1 if not use
-        panel_config.rgb_endian = LCD_RGB_ENDIAN_BGR;           //LCD_RGB_ENDIAN_RGB;
-        panel_config.bits_per_pixel = 16;                       // Implemented by LCD command `3Ah` (16/18)
-
-        ESP_ERROR_CHECK(esp_lcd_new_panel_gc9a01(io_handle, &panel_config, &panel_handle));
-        ESP_ERROR_CHECK(esp_lcd_panel_reset(panel_handle));
-        ESP_ERROR_CHECK(esp_lcd_panel_init(panel_handle));
-        ESP_ERROR_CHECK(esp_lcd_panel_invert_color(panel_handle, true));
-        ESP_ERROR_CHECK(esp_lcd_panel_mirror(panel_handle, true, false));
-        ESP_ERROR_CHECK(esp_lcd_panel_disp_on_off(panel_handle, true)); 
-
-        display_ = new SpiLcdDisplay(io_handle, panel_handle,
-                                    DISPLAY_WIDTH, DISPLAY_HEIGHT, DISPLAY_OFFSET_X, DISPLAY_OFFSET_Y, DISPLAY_MIRROR_X, DISPLAY_MIRROR_Y, DISPLAY_SWAP_XY);
     }
 
     void InitializeButtons() {
@@ -169,13 +89,11 @@ private:
 
 public:
     KalicyhS3ML307() : DualNetworkBoard(ML307_TX_PIN, ML307_RX_PIN, GPIO_NUM_NC),
-        boot_button_(BOOT_BUTTON_GPIO) {  
+        boot_button_(BOOT_BUTTON_GPIO) {
+        InitializePowerSaveTimer();
         InitializePowerManager();
         InitializeCodecI2c();
-        InitializeSpi();
-        InitializeGc9a01Display();
         InitializeButtons();
-        GetBacklight()->RestoreBrightness();
     }
 
     virtual Led* GetLed() override {
@@ -184,12 +102,11 @@ public:
     }
 
     virtual Display* GetDisplay() override {
-        return display_;
+        return nullptr;  // 不使用屏幕
     }
-    
+
     virtual Backlight* GetBacklight() override {
-        static PwmBacklight backlight(DISPLAY_BACKLIGHT_PIN, DISPLAY_BACKLIGHT_OUTPUT_INVERT);
-        return &backlight;
+        return nullptr;  // 不使用背光
     }
 
     virtual AudioCodec* GetAudioCodec() override {
@@ -208,13 +125,6 @@ public:
 
     // 重写 DualNetworkBoard 的方法，代理到当前活动的板子
     virtual void StartNetwork() override {
-        auto display = GetDisplay();
-    
-        if (GetNetworkType() == NetworkType::WIFI) {
-            display->SetStatus(Lang::Strings::CONNECTING);
-        } else {
-            display->SetStatus(Lang::Strings::DETECTING_MODULE);
-        }
         DualNetworkBoard::StartNetwork();
     }
 
